ea11dfba3b1477297b38f830681d3147e8738546
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-04-04 19:20:23 -0400

    finish basic bootstrap styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-
+import Card from 'react-bootstrap/Card'
 
 
 export default class MovieView extends Component {
@@ -21,22 +21,21 @@ export default class MovieView extends Component {
         render() {
                 const { movie, onBackClick } = this.props;
                 return (
-                        <div className="movie-view">
-                                <div className="movie-poster">
-                                        <img src={`https://res.cloudinary.com/drghkywbx/image/upload/v1649103136/${movie.ImagePath}`} crossOrigin="anonymous" />
-                                </div>
-                                <div className="movie-title">
-                                        <span className="label">Title: </span>
-                                        <span className="value">{movie.Title}</span>
-                                </div>
-                                <div className="movie-description">
-                                        <span className="label">Description: </span>
-                                        <span className="value">{movie.Description}</span>
-                                </div>
-                                <button type="button" onClick={() => onBackClick(null)}>
+                      
+                        <>
+                        <Card>
+                          <Card.Img className='movie-poster' variant="top" src={`https://res.cloudinary.com/drghkywbx/image/upload/v1649103136/${movie.ImagePath}`} crossOrigin="anonymous" />
+                          <Card.Body>
+                            <Card.Text>
+                                <h2 className='movie-title'>{movie.Title}</h2>
+                              {movie.Description}
+                            </Card.Text>
+                            <button type="button" onClick={() => onBackClick(null)}>
                                         Back
                                 </button>
-                        </div>
+                          </Card.Body>
+                        </Card>
+                      </>
                 );
         }
 }
