a8435feda80e5e226aa225a6faf23569bd40f3e3
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-04-05 18:15:43 -0400

    add validation to login and registration

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,10 +3,34 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import RegistrationView from '../registration-view/registration-view';
 
 function LoginView({ onLoggedIn }) {
         const [username, setUsername] = useState('');
         const [password, setPassword] = useState('');
+
+        const [usernameErr, setUsernameErr] = useState('');
+        const [passwordErr, setPasswordErr] = useState('');
+
+        const validate = () => {
+                let isReq = true;
+                if (!username) {
+                        setUsernameErr('Username Required');
+                        isReq = false;
+                } else if (username.length < 2) {
+                        setUsernameErr('Username must be at least 2 characters long');
+                        isReq = false;
+                }
+                if (!password) {
+                        setPasswordErr('Password Required');
+                        isReq = false;
+                } else if (password.length < 6) {
+                        setPasswordErr('Password must be at least 6 characters');
+                        isReq = false;
+                }
+
+                return isReq;
+        };
 
         const handleChange = (e) => {
                 console.log(e.target.name);
@@ -18,18 +42,22 @@ function LoginView({ onLoggedIn }) {
 
         const handleSubmit = (e) => {
                 e.preventDefault();
-                // Send authentication request to the server
-                axios.post('https://myflix-api-cgray.herokuapp.com/login', {
-                        Username: username,
-                        Password: password,
-                })
-                        .then((response) => {
-                                const { data } = response;
-                                onLoggedIn(data);
+                // Check form validation returns boolean
+                const isReq = validate();
+                if (isReq) {
+                        // Send authentication request to the server
+                        axios.post('https://myflix-api-cgray.herokuapp.com/login', {
+                                Username: username,
+                                Password: password,
                         })
-                        .catch((e) => {
-                                console.log('No user found');
-                        });
+                                .then((response) => {
+                                        const { data } = response;
+                                        onLoggedIn(data);
+                                })
+                                .catch((e) => {
+                                        console.log('No user found');
+                                });
+                }
         };
 
         return (
@@ -39,16 +67,19 @@ function LoginView({ onLoggedIn }) {
                                 <Form.Group controlId="formUsername">
                                         <Form.Label>Username:</Form.Label>
                                         <Form.Control name="username" type="text" onChange={handleChange} />
+                                        {usernameErr ? <span>{usernameErr}</span> : ''}
                                 </Form.Group>
 
                                 <Form.Group controlId="formPassword">
                                         <Form.Label>Password:</Form.Label>
                                         <Form.Control name="password" type="password" onChange={handleChange} />
+                                        {passwordErr ? <span>{passwordErr}</span> : ''}
                                 </Form.Group>
                                 <Button variant="primary" type="submit" onClick={handleSubmit}>
                                         Submit
                                 </Button>
                         </Form>
+                        <RegistrationView />
                 </div>
         );
 }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -10,30 +10,90 @@ export default function RegistrationView() {
                 Birthday: '',
         });
 
+        const [validationErrors, setValidationErrors] = useState({
+                UsernameErr: '',
+                PasswordErr: '',
+                EmailErr: '',
+                BirthdayErr: '',
+        });
+
+        const checkNotEmptySetError = (keyName, message, isReq) => {
+                if (!userInfo[keyName]) {
+                        setValidationErrors((prevErrors) => ({ ...prevErrors, [`${keyName}Err`]: message }));
+                        return false;
+                }
+                // Reset error when requirements are met
+                setValidationErrors((prevErrors) => ({ ...prevErrors, [`${keyName}Err`]: '' }));
+
+                // Checks if any validation before has already been false
+                if (!isReq) return false;
+                // Return true when pass validation
+                return true;
+        };
+
+        const checkValidEmail = (isReq) => {
+                const regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+                const isValid = regex.test(userInfo.Email);
+                if (!isValid) {
+                        setValidationErrors((prevErrors) => ({ ...prevErrors, EmailErr: 'Not a valid email' }));
+                }
+                // Check for previous validation faliure
+                if (!isReq) return false;
+                return isValid;
+        };
+
+        const validate = () => {
+                let isReq = true;
+
+                // Get array of keys in the userInfo state
+                const fields = Object.keys(userInfo);
+                // Loop through keys and validate each has data
+                fields.forEach((field) => {
+                        isReq = checkNotEmptySetError(field, `${field} is required`, isReq);
+                });
+
+                // Check for email
+                if (userInfo.Email) {
+                        isReq = checkValidEmail(isReq);
+                }
+
+                console.log(isReq);
+                return isReq;
+        };
+
         const handleChange = (e) => {
-                console.log(userInfo);
                 const { name, value } = e.target;
                 setUserInfo({ ...userInfo, [name]: value });
         };
 
+        const handleSubmit = (e) => {
+                e.preventDefault();
+                const isReq = validate();
+                console.log(isReq, validationErrors);
+        };
+
         return (
-                <Form>
+                <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="formUsername">
                                 <Form.Label>Username:</Form.Label>
-                                <Form.Control name="username" type="text" onChange={handleChange} />
+                                <Form.Control name="Username" type="text" onChange={handleChange} />
+                                {validationErrors.UsernameErr ? <span>{validationErrors.UsernameErr}</span> : ''}
                         </Form.Group>
 
                         <Form.Group controlId="formPassword">
                                 <Form.Label>Password:</Form.Label>
-                                <Form.Control name="password" type="password" onChange={handleChange} />
+                                <Form.Control name="Password" type="password" onChange={handleChange} />
+                                {validationErrors.PasswordErr ? <span>{validationErrors.PasswordErr}</span> : ''}
                         </Form.Group>
-                        <Form.Group controlId="formPassword">
+                        <Form.Group controlId="formEmail">
                                 <Form.Label>Email:</Form.Label>
-                                <Form.Control name="email" type="email" onChange={handleChange} />
+                                <Form.Control name="Email" type="email" onChange={handleChange} />
+                                {validationErrors.EmailErr ? <span>{validationErrors.EmailErr}</span> : ''}
                         </Form.Group>
-                        <Form.Group controlId="formPassword">
+                        <Form.Group controlId="formBirthday">
                                 <Form.Label>Date of Birth:</Form.Label>
-                                <Form.Control name="birthday" type="date" onChange={handleChange} />
+                                <Form.Control name="Birthday" type="date" onChange={handleChange} />
+                                {validationErrors.BirthdayErr ? <span>{validationErrors.BirthdayErr}</span> : ''}
                         </Form.Group>
                         <Button variant="primary" type="submit">
                                 Submit
