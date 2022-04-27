8df4c58f9a2a47816664ee0cc79e62f0006c4ebe
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-04-27 16:50:03 -0400

    add build command

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
 import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp, devToolsEnhancer());
+const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
